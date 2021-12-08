b9a201133062f745cadc7c3aed2995b84b47537f
Vladimir Leveryev <volodya.leveryev@gmail.com>
2021-12-09 06:51:25 +0900

    Улучшения в системе сборки проекта

--- site/webpack.config.js
+++ site/webpack.config.js
@@ -1,31 +1,51 @@
-const Dotenv = require('dotenv-webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const fs = require('fs')
 const path = require('path')
+const webpack = require('webpack')
 
-module.exports = {
-  mode: 'development',
-  entry: path.resolve(__dirname, 'src', 'main.jsx'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      options: {
-        presets: [
-          '@babel/preset-env',
-          '@babel/preset-react'
+module.exports = (env, argv) => {
+  // Если продакшн-сборка тогда читаем URL к API из файла
+  let api_url = 'http://localhost:8000'
+  if (argv.mode === 'production') {
+    try {
+      api_url = fs.readFileSync('api_url.txt')
+    } catch (e) {
+      console.log('api_url.txt is missing!')
+      process.exit(1)
+    }
+  }
+
+  return {
+    mode: argv.mode || 'development',
+    entry: path.resolve(__dirname, 'src', 'main.jsx'),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react'
+          ]
+        }
+      }]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          API_URL: JSON.stringify(api_url)
+        }
+      }),
+      new CopyPlugin({
+        patterns: [
+          {from: path.resolve(__dirname, 'src', 'index.html')}
         ]
-      }
-    }]
-  },
-  plugins: [
-    new Dotenv(),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html')
-    })
-  ]
+      })
+    ]
+  }
 }
