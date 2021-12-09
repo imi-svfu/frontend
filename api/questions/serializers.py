7b45034573b9e1f3de0db064789bbd95def2ee60
Vladimir Leveryev <volodya.leveryev@gmail.com>
2021-12-10 08:39:28 +0900

    Выделение бэкенда в отдельный репозиторий

--- api/questions/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-
-from .models import Question
-
-
-class QuestionSerializer(ModelSerializer):
-    class Meta:
-        model = Question
-        fields = '__all__'
