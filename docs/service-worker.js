5ee389b986a4027e52366f1ac7b53e718734ea58
Vladimir Leveryev <volodya.leveryev@gmail.com>
2022-03-25 10:30:38 +0900

    Switch to react-scripts

--- docs/service-worker.js
+++
@@ -1,1 +0,0 @@
-self.addEventListener("install",(e=>{console.log("Install event!")})),self.addEventListener("activate",(e=>{console.log("Activate event!")})),self.addEventListener("fetch",(e=>{console.log("Fetch intercepted for:",e.request.url)}));
